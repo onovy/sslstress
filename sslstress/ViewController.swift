dec7dd3b66c0afa0b4a9e8a1c5779f3f6495d83f
Tomas Hluchan <tomas.hluchan@gmail.com>
2016-10-25 09:49:55 +0200

    Initial Commit

---
+++ sslstress/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sslstress
+//
+//  Created by Tomas Hluchan on 25/10/2016.
+//  Copyright © 2016 Tomas Hluchan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
